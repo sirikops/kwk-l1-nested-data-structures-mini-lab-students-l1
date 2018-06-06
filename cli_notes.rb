1c1fc8abe327cf0b61db7853fb23866a4136a3e2
sirikops <sirikops@hotmail.com>
2018-06-06 22:23:40 +0000

    automatically backed up by learn

--- cli_notes.rb
+++ cli_notes.rb
@@ -5,9 +5,9 @@
 
 game_number = "75"
 
-puts "Please enter your first name."
+puts "Please enter your first name:"
 first_name = gets.chomp
-puts "Please enter your last name."
+puts "Please enter your last name:"
 last_name = gets.chomp
 
 puts "Welcome to the #{game_number}th Hunger Games, #{first_name} #{last_name} "
